3d88a7e2a64e47e86cf321fd4d2aa8c7cecabdc4
Jagmohan Singh <jattiphrswan49@gmail.com>
2025-03-21 11:35:11 +0530

    first commit

---
+++ script .js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    menuIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+});
